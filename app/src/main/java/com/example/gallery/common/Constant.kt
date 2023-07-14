9ec8f7f8e564ff288a469f19d93720a75e94508a
theMIslam <valvekakaxa@gmail.com>
2023-07-14 13:19:41 +0600

    added apikey + mainviewmodel

--- app/src/main/java/com/example/gallery/common/Constant.kt
+++ app/src/main/java/com/example/gallery/common/Constant.kt
@@ -2,6 +2,6 @@ package com.example.gallery.common
 
 
 object Constant {
-//    const val PART = "snippet,contentDetails"
-//    const val CHANNEL_ID = "UCZW5lIUz93q_aZIkJPAC0IQ"
+    const val ACCESS_KEY = "Ic4bCb5Ht5dTnK7pwROtRbJUyBWpLf7fLT_hkZMR-MQ"
+    const val SECRET_KEY = "um30g-0TH-87D3NMKOKS3uPmUBwPO_4UEkBF8U6WUx8"
 }
